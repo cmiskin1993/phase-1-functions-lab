50982b5e4f8040134581e1713d4b5699954b8d92
Carrie Miskin <clove40@gmail.com>
2022-02-27 13:01:55 -0500

    commit tree da6b065be01397ccf4dca884ae8eeae7746a0062

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+
+    if (42 > someValue) {
+    return 42 - someValue; 
+  } else {
+    return someValue - 42
+  }
+}
+  
+ function distanceFromHqInFeet(someValue) {
+  let distance = distanceFromHqInBlocks (someValue) * 264
+  return distance
+    }
+
+  function distanceTravelledInFeet(start, destination) {
+    let distance = Math.abs(start - destination) * 264
+  return distance
+      }
+
+  function calculatesFarePrice(start, destination) {
+    let blockDistance = Math.abs(start - destination) * 264
+    let distance = blockDistance
+
+      if (distance <= 400) {
+      return 0
+
+      } else if (distance > 400 && distance < 2000) {
+        return (distance - 400) * .02
+      
+      } else if (distance > 2000 && distance < 2500) {
+        return 25;
+
+      } else if (distance >= 2500) {
+        return "cannot travel that far"
+      }
+    }
